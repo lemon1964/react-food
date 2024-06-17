94304c0c77ea62b87ff3e7bc82a6f515fcee31ec
lemon <lemon@example.com>
2024-06-17 03:27:23 +0300

    commit 17.06_2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           © {new Date().getFullYear()} Copyright Text
           <a
             className="grey-text text-lighten-4 right"
-            href="https://github.com/lemon1964/react-shop"
+            href="https://github.com/lemon1964/react-food"
             rel="noreferrer"
             target="_blank"
           >
